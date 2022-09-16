228dfd0d14004c39c31e8221398d7e24fcabfb88
Julia Stensnes-Martini <juliaest@ntnu.no>
2022-09-16 15:51:00 +0200

    La til testmappe og fil, samt la til dependencies i pom fil. Plugins gir error når jeg prøver å kjÃjøre test

---
+++ notpaint/src/test/java/gr2213/Brushes/SquareBrushTest.java
@@ -0,0 +1,22 @@
+package gr2213.Brushes;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import javafx.scene.canvas.Canvas;
+
+public class SquareBrushTest {
+
+    Canvas canvas;
+
+    @Test
+    @DisplayName("Test if the brush is created.")
+    public void testGetPixels() {
+        SquareBrush brush = new SquareBrush(5);
+        brush.GetPixels(canvas, 2, 4);
+        assertEquals(brush.GetPixels(canvas, 2, 4).anyMatch(p -> p.getKey() == 2 && p.getValue() == 4), true);
+    }
+
+}
