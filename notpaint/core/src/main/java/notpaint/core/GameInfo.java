cc073872b0e0b926c87a52ccf5a0538ebfb2a117
Andrine Heggelund <andrihe@stud.ntnu.no>
2022-10-07 15:18:16 +0200

    Added checkstyle and spotbugs

--- notpaint/core/src/main/java/notpaint/core/GameInfo.java
+++ notpaint/core/src/main/java/notpaint/core/GameInfo.java
@@ -8,27 +8,31 @@ import java.util.Random;
 import java.util.UUID;
 
 /**
- * One instance of the GameInfo class represents a Game (either ongoing or finished)
+ * One instance of the GameInfo class represents a Game (either ongoing or
+ * finished)
  */
 public class GameInfo {
 
-    private int maxIterations;      
+    private int maxIterations;
     private int secondsPerRound;
     private boolean newWordEachRound;
 
     private int currentIterations;
     private List<String> words;
     private String lastEditor; // person who last edited
-    
+
     private UUID uuid;
 
     private static Random random;
 
     /**
      * 
-     * @param maxIterations The number of iterations before this game is finished
-     * @param secondsPerRound How many seconds the users should have to draw each iteration
-     * @param newWordEachRound Whether there should be generated a new word each round. If false there will not be a new word after an iteration.
+     * @param maxIterations    The number of iterations before this game is finished
+     * @param secondsPerRound  How many seconds the users should have to draw each
+     *                         iteration
+     * @param newWordEachRound Whether there should be generated a new word each
+     *                         round. If false there will not be a new word after an
+     *                         iteration.
      */
     public GameInfo(int maxIterations, int secondsPerRound, boolean newWordEachRound) {
         if (maxIterations < 1)
@@ -44,36 +48,42 @@ public class GameInfo {
         this.lastEditor = "N/A";
 
         generateNewWord();
-        
+
         uuid = UUID.randomUUID();
     }
-    
+
     /**
-     * Add a new iteration to the Game. Generates a new word if newWordEachRound was set to true
-     * @param editor The nickname of the person who edited the image for this iteration
+     * Add a new iteration to the Game. Generates a new word if newWordEachRound was
+     * set to true
+     * 
+     * @param editor The nickname of the person who edited the image for this
+     *               iteration
      */
-    public void addIteration(String editor) { // last editor + counter 
+    public void addIteration(String editor) { // last editor + counter
         this.lastEditor = editor;
-        
+
         increaseCurrentIterations();
 
-        if(!isFinished()) {
-            if(newWordEachRound)            
+        if (!isFinished()) {
+            if (newWordEachRound)
                 generateNewWord();
         }
     }
 
     /**
      * 
-     * @return String representing the path to image. Could be a file or web address.
+     * @return String representing the path to image. Could be a file or web
+     *         address.
      */
     public String getImagePath() {
-        // TODO: Determine location where this will be saved. For now will be saved relative to where program is running from
+        // TODO: Determine location where this will be saved. For now will be saved
+        // relative to where program is running from
         return uuid.toString() + ".png";
     }
 
     /**
      * Chech if the game is finished (currentIterations has reached maxIterations)
+     * 
      * @return true if finished, false if not
      */
     public boolean isFinished() {
@@ -81,7 +91,7 @@ public class GameInfo {
     }
 
     private void increaseCurrentIterations() {
-        if(currentIterations + 1 > maxIterations)
+        if (currentIterations + 1 > maxIterations)
             throw new UnsupportedOperationException("Cannot increase currentIterations over maxIterations");
         currentIterations++;
     }
@@ -90,18 +100,18 @@ public class GameInfo {
         return currentIterations;
     }
 
-    
     private void generateNewWord() {
         // get a new random word from a text file (in resources)
         String wordListString = null;
-        try{
+        try {
             wordListString = new String(getClass().getClassLoader().getResourceAsStream("words.txt").readAllBytes());
-        } catch(IOException exception) {
+        } catch (IOException exception) {
             exception.printStackTrace();
         }
-        var wordListArray =  wordListString.split("\\r?\\n|\\r");
+        var wordListArray = wordListString.split("\\r?\\n|\\r");
 
-        if(random == null) random = new Random();
+        if (random == null)
+            random = new Random();
         int randomIndex = random.nextInt(wordListArray.length);
 
         // Add the word to the list of words
@@ -111,6 +121,7 @@ public class GameInfo {
 
     /**
      * Returns the word that is currently to be drawn.
+     * 
      * @return
      */
     public String getWord() {
@@ -124,6 +135,5 @@ public class GameInfo {
     public int getSecondsPerRound() {
         return secondsPerRound;
     }
-
 
 }
