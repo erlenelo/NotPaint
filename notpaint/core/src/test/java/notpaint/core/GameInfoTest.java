96a31346c2c95018faafdd358e0f7965f5f37c7e
Erlend EllingbÃ Løvaa <erlenelo@stud.ntnu.no>
2022-10-12 11:17:03 +0200

    Added all tests for core, still need logic for most.

---
+++ notpaint/core/src/test/java/notpaint/core/GameInfoTest.java
@@ -0,0 +1,43 @@
+package notpaint.core;
+
+import org.junit.jupiter.api.Test;
+
+public class GameInfoTest {
+    @Test
+    void testAddIteration() {
+
+    }
+
+    @Test
+    void testGetCurrentIterations() {
+
+    }
+
+    @Test
+    void testGetImagePath(){
+
+    
+
+
+    }
+
+    @Test
+    void testGetLastEditor() {
+
+    }
+
+    @Test
+    void testGetSecondsPerRound() {
+
+    }
+
+    @Test
+    void testGetWord() {
+
+    }
+
+    @Test
+    void testIsFinished() {
+
+    }
+}
---
+++ notpaint/core/src/test/java/notpaint/core/PaintSettingsTest.java
@@ -0,0 +1,25 @@
+package notpaint.core;
+
+import org.junit.jupiter.api.Test;
+
+public class PaintSettingsTest {
+    @Test
+    void testGetBrush() {
+
+    }
+
+    @Test
+    void testGetColor() {
+
+    }
+
+    @Test
+    void testSetBrush() {
+
+    }
+
+    @Test
+    void testSetColor() {
+
+    }
+}
