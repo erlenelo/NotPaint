8a2e0612f9aa9b835f3d8631e0f8c288febbdc5e
Erlend EllingbÃ Løvaa <erlenelo@stud.ntnu.no>
2022-10-12 17:04:47 +0200

    Added tests for GameInfo, currently 83% test coverage

---
+++ notpaint/core/src/test/java/notpaint/core/Persistence/GameInfoPersistenceTest.java
@@ -0,0 +1,25 @@
+package notpaint.core.Persistence;
+
+import org.junit.jupiter.api.Test;
+
+public class GameInfoPersistenceTest {
+    @Test
+    public void testGetActiveGameInfo() {
+
+    }
+
+    @Test
+    public void testGetAllGameInfos() {
+
+    }
+
+    @Test
+    public void testSaveGameInfo() {
+
+    }
+
+    @Test
+    public void testSetActiveGameInfo() {
+
+    }
+}
