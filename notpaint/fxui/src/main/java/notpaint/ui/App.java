053cf3db0db04d5f52d646d4a728247791fec9c8
Julia Stensnes-Martini <juliaest@ntnu.no>
2022-10-14 11:33:30 +0200

    Fixed spotbug warning

--- notpaint/fxui/src/main/java/notpaint/ui/App.java
+++ notpaint/fxui/src/main/java/notpaint/ui/App.java
@@ -13,7 +13,7 @@ import java.io.IOException;
  */
 public class App extends Application {
 
-    public static Scene scene;
+    static Scene scene;
 
     @Override
     public void start(Stage stage) throws IOException {
