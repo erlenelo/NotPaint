a42e9a9a7609e496b900c3ef3fdbcd5abd3eda08
Julia Stensnes-Martini <juliaest@ntnu.no>
2022-11-11 13:50:11 +0100

    Fixed issue with testDoneButton in UsernameSelectControllerTest by adding a functions that deletes the username file before running the tests.

--- notpaint/fxui/src/test/java/notpaint/ui/UsernameSelectControllerTest.java
+++ notpaint/fxui/src/test/java/notpaint/ui/UsernameSelectControllerTest.java
@@ -8,6 +8,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.RadioButton;
 import javafx.stage.Stage;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit5.ApplicationTest;
 import org.testfx.util.WaitForAsyncUtils;
@@ -22,29 +24,43 @@ public class UsernameSelectControllerTest extends ApplicationTest {
 
     @Override
     public void start(Stage stage) throws Exception {
+        // System.out.println("UsernameSelectControllerTest start");
         FXMLLoader fxmlLoader = new FXMLLoader(
-            UsernameSelectController.class.getResource("UsernameSelectView.fxml"));
+                UsernameSelectController.class.getResource("UsernameSelectView.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         App.scene = scene;
         scene.getStylesheets().add(getClass().getResource("fxui.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
         controller = fxmlLoader.getController();
+        // System.out.println("Controller: " + controller.getClass().toString());
     }
 
     // Test that the controller is created.
     @Test
     public void testController() {
         assertNotNull(controller);
+        System.out.println("testController passed");
+    }
+
+    @BeforeAll
+    public static void deleteFile() {
+        GameSelectController ctrl = new GameSelectController();
+        try {
+            ctrl.deleteUsername();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     // Test if clicking on the Done-button opens GameSelectView.
     @Test
     public void testDoneButton() throws InterruptedException {
+
         clickOn("#setUsernameTextField");
         write("testAuthor");
-        clickOn("#createUsername");
-        WaitForAsyncUtils.waitForFxEvents();
+        clickOn("#doneButton");
+        // WaitForAsyncUtils.waitForFxEvents();
         assertNotNull(findSceneRootWithId("gameSelectRoot"));
     }
 
@@ -57,12 +73,11 @@ public class UsernameSelectControllerTest extends ApplicationTest {
     public void testRadioButtonsHaveRightValue() {
         RadioButton yesRadioButton = (RadioButton) lookup("#yesRadioButton").query();
         RadioButton noRadioButton = (RadioButton) lookup("#noRadioButton").query();
-        clickOn("#checkboxNo");
+        clickOn("#noRadioButton");
         assertTrue(noRadioButton.isSelected());
         assertFalse(yesRadioButton.isSelected());
-        clickOn("#checkboxYes");
+        clickOn("#yesRadioButton");
         assertTrue(yesRadioButton.isSelected());
     }
 
-    
 }
