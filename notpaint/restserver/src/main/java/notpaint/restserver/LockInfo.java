6d9e360e3b1c775fa01987febfb34da1203506a3
Sofus Emil Simonsen Øvretveit <seovretv@stud.ntnu.no>
2022-11-17 13:01:50 +0100

    Add equals ovveride for LockInfo and ensure compareTo adherance

--- notpaint/restserver/src/main/java/notpaint/restserver/LockInfo.java
+++ notpaint/restserver/src/main/java/notpaint/restserver/LockInfo.java
@@ -14,8 +14,22 @@ class LockInfo implements Comparable<LockInfo> {
 
     @Override
     public int compareTo(LockInfo o) {
-        return lockUntil.compareTo(o.lockUntil);
+        int lockUntilCompare = lockUntil.compareTo(o.lockUntil);
+        if (lockUntilCompare != 0) {
+            return lockUntilCompare;
+        } else {
+            return uuid.compareTo(o.uuid);
+        }
     }    
 
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof LockInfo)) {
+            return false;
+        }
+
+        LockInfo other = (LockInfo) obj;
+        return uuid.equals(other.uuid) && lockUntil.equals(other.lockUntil);
+    }
     
 }
