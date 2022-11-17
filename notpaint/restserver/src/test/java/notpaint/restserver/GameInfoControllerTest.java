3d0c42b7dd1f42497e50574a6727ec5be9195b21
Sofus Emil Simonsen Øvretveit <seovretv@stud.ntnu.no>
2022-11-17 15:42:25 +0100

    Add some GameInfoController tests

--- notpaint/restserver/src/test/java/notpaint/restserver/GameInfoControllerTest.java
+++ notpaint/restserver/src/test/java/notpaint/restserver/GameInfoControllerTest.java
@@ -1,17 +1,22 @@
 package notpaint.restserver;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.UUID;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
 
-
-
+import notpaint.persistence.GameInfo;
 import notpaint.persistence.LocalGameInfoPersistence;
 
 /**
@@ -41,7 +46,45 @@ public class GameInfoControllerTest {
     }
 
     @Test
-    public void testGetAllGameInfos() {
+    public void testSaveAndGetGameInfos() {
+        var gameInfo1 = new GameInfo(5, 5, false);
+        var gameInfo2 = new GameInfo(2, 5, true);
+        controller.putSaveGameInfo(gameInfo1);
+        controller.putSaveGameInfo(gameInfo2);
+        
+        ResponseEntity<List<GameInfo>> allGameInfosResponse = controller.getAllGameInfos();
+        assertTrue(allGameInfosResponse.hasBody());
+        
+        var allGameInfos = allGameInfosResponse.getBody();
+        if (allGameInfos == null) {
+            throw new AssertionError("Response body is null");
+        }
+
+        assertTrue(allGameInfos.contains(gameInfo1));
+        assertTrue(allGameInfos.contains(gameInfo2));
+    }
+
+    @Test
+    public void testGameInfoLocking() {
+        var gameInfo = new GameInfo(5, 100, false);
+        controller.putSaveGameInfo(gameInfo);
+
+        Boolean isLocked = controller.getLockStatus(gameInfo.getUuid()).getBody();
+
+        if (isLocked == null) {
+            throw new AssertionError("Response body is null");
+        }
+        assertFalse(isLocked);
+
+        Boolean lockSucessfull = controller.postRequestLock(gameInfo.getUuid()).getBody();
+
+        if (lockSucessfull == null) {
+            throw new AssertionError("Response body is null");
+        }
+        
+
+
+
 
     }
 
